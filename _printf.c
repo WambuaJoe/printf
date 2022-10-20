c0b95f5b6bdc65d80a31c7e52f1f73409d79ab64
Modasoka <kaytujohn254@gmail.com>
2022-10-21 01:03:51 +0300

    new

--- _printf.c
+++ _printf.c
@@ -1,1 +1,10 @@
-//this function serves to recreate the printf function in C programming
+#include "main.h"
+
+/*
+ * _printf - function that produces output according to a 
+ *           specified format
+ * @format: character string composed of zero, or more, directives
+ *
+ * Return: returns the length of the output
+ */
+
