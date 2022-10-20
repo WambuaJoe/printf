8ab3db15cf8d910f7a804c22c964624cfdbb7bb5
Modasoka <kaytujohn254@gmail.com>
2022-10-20 16:47:28 -0700

    function that produces an output according to the specified format

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- *_printf - function that produces output according to a 
+ * _printf - function that produces output according to a
  *           specified format
  * @format: character string composed of zero, or more, directives
  *
@@ -10,16 +10,17 @@
 
 int _printf(const char *format, ...)
 {
-  va_list arguments;
-  printf specifier[] = {
-			{"c", char_printf},
-			{"s", _string_printf},
-			{"i", _int_print},
-			{"d", _print_dec},
-  };
-  int i = 0, j = 0;
-  int len_specifier;
+	va_list arguments;
+	printf specifier[] = {
+		{"c", char_printf},
+		{"s", _string_printf},
+		{"i", _int_print},
+		{"d", _print_dec},
+	};
+	int i = 0, j = 0;
+	int len_specifier;
 
-  va_start(arguments, format);
-  len_specifier = sizeof(specifier) / sizeof(specifier[0]);
+	va_start(arguments, format);
+
+	len_specifier = sizeof(specifier) / sizeof(specifier[0]);
 }
